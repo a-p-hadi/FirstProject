dfdf6dfb42bd025b2c0ba040b4c14c796b786f27
a.p.hadi <131952146+a-p-hadi@users.noreply.github.com>
2023-06-21 16:02:35 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,1 @@
-import json
 
-
-def formatter(string, sort_keys=True, indent=4):
-    # load incoming string into JSON
-    loaded_json = json.loads(string)
-    # dump as string
-    return json.dumps(loaded_json, sort_keys=sort_keys, indent=indent)
